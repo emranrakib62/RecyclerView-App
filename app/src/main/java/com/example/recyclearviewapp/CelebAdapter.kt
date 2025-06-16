a3a1878a7137f831822f138c5757a94b35ecc487
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-16 18:13:19 +0600

    updated

--- app/src/main/java/com/example/recyclearviewapp/CelebAdapter.kt
+++ app/src/main/java/com/example/recyclearviewapp/CelebAdapter.kt
@@ -1,4 +1,20 @@
 package com.example.recyclearviewapp
 
-class CelebAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ListAdapter
+
+class CelebAdapter :ListAdapter<Celeb,CelebViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CelebViewHolder {
+
+
+    }
+
+    override fun onBindViewHolder(holder: CelebViewHolder, position: Int) {
+
+    }
+
+
+
 }
