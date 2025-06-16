e44e4c141150281055cbed80d64e658b222e27b2
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-16 18:04:42 +0600

    updated

---
+++ app/src/main/java/com/example/recyclearviewapp/CelebAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.recyclearviewapp
+
+class CelebAdapter {
+}
---
+++ app/src/main/java/com/example/recyclearviewapp/CelebViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.recyclearviewapp
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclearviewapp.databinding.CelebBinding
+
+class CelebViewHolder (val binding: CelebBinding) :RecyclerView.ViewHolder(binding.root) {
+}
