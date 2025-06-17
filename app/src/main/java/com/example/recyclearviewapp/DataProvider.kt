0bc71f9e3672b14176b4b680d8affeeac4b68b38
Emranul Hoque <emranrakib01862@gmail.com>
2025-06-17 18:41:14 +0600

    updated

--- app/src/main/java/com/example/recyclearviewapp/DataProvider.kt
+++ app/src/main/java/com/example/recyclearviewapp/DataProvider.kt
@@ -1,6 +1,6 @@
 package com.example.recyclearviewapp
 const val priyankabio="Priyanka Chopra Jonas (née Chopra) was born on July 18, 1982 in Jamshedpur, India, to the family of Capt. Dr. Ashok Chopra and Dr. Madhu Chopra, both Indian Army physicians. She had a very varied upbringing. She started her education at La Martinière Girls College in Lucknow as a resident student; "
-const val profileimg="https://www.imdb.com/name/nm1231899/mediaviewer/rm2819207936/?ref_=nm_ov_ph"
+const val profileimg="https://m.media-amazon.com/images/M/MV5BMjAxNzUwNjExOV5BMl5BanBnXkFtZTcwNDUyMTUxNw@@._V1_.jpg"
 
 
 const val annabio=" the daughter of actor Chunky Panday, made her acting debut with the 2019 film Student of the Year 2. In 2023, she made her mark in the industry with acclaimed performances in movies and shows such as Kesari Chapter 2 and Call Me Bae. Garnering praise for her versatile roles, Ananya has quickly risen to prominence and is widely regarded as one of the most popular young actresses in Bollywood."
